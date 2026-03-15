06ecbe3b2a5e7b40d5015df240e64a659b141207
ikrambousfiha <ikramobousfiha@gmail.com>
2026-03-16 00:31:34 +0800

    fvegeq

--- df/back/settings.py
+++ df/back/settings.py
@@ -105,9 +105,6 @@ DEFAULT_FROM_EMAIL = 'elmansourabwab@gmail.com'
 ADMIN_EMAIL = 'elmansourabwab@gmail.com'  
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://localhost:5174",
-    "http://127.0.0.1:5173",
-    "http://127.0.0.1:5174",
+    "*"
 ]
 CORS_ALLOW_CREDENTIALS = True
